6e55a0e719f6e765cb0bed7d6d37e534fb5e6495
Алексеева Анастасия Николаевна <analekseeva_1@edu.hse.ru>
2017-08-05 13:08:05 +0500

    оптимизировала код

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using static System.Console;
 
 namespace Task11
 {
-    class Program
+    class Action
     {
         public static readonly string alphabetRU = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
         public static readonly string alphabetENG = "abcdefghijklmnopqrstuvwxyz";
+        public static readonly string symbols = @"!&.,?/\*^$#@№;%:()-_=+1234567890";
 
         public static char Encode(char c, int n, bool lang)
         {
@@ -22,17 +24,17 @@ namespace Task11
             {
                 try
                 {
-                    number = Convert.ToInt32(Console.ReadLine());
+                    number = Convert.ToInt32(ReadLine());
                     ok = true;
                 }
                 catch (FormatException)
                 {
-                    Console.WriteLine("Ошибка при вводе числа");
+                    WriteLine("Ошибка при вводе числа");
                     ok = false;
                 }
                 catch (OverflowException)
                 {
-                    Console.WriteLine("Ошибка при вводе числа");
+                    WriteLine("Ошибка при вводе числа");
                     ok = false;
                 }
             } while (!ok);
@@ -42,35 +44,45 @@ namespace Task11
             return number;
         }
 
-        static void Main(string[] args)
+        public static string Encryption(string text, int n)
         {
-            Console.WriteLine("Введите текст:");
-            string text = Console.ReadLine();
-
-            Console.WriteLine("Введите число N (Количество символов, на которое сдвинется шифруемый текст. " +
-                              "\nПоложительное число - сдвиг вправо), отрицательное - сдвиг влево):");
-            int n = Input();
-            char[] mem = text.ToCharArray();
             string newtxt = String.Empty;
-            string symbols = @"!&.,?/\*^$#@№;%:()-_=+1234567890";
+            
             bool lang = true;
-            foreach (char t in mem)
+            foreach (char t in text)
             {
-                if (!alphabetRU.Contains(t.ToString()))
+                if (!alphabetRU.Contains(t.ToString())) // определение алфавита
                 {
                     lang = false;
                     break;
                 }
             }
 
-            foreach (char t in mem)
+            foreach (char t in text) // зашифровка
             {
                 if (t != ' ' || !symbols.Contains(t.ToString()))
                     newtxt += Encode(t, n, lang);
                 else newtxt += t;
             }
-            Console.WriteLine(newtxt);
-            Console.Read();
+
+            return newtxt;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите текст:");
+            string text = ReadLine();
+
+            WriteLine("Введите число N (Количество символов, на которое сдвинется шифруемый текст. " +
+                              "\nПоложительное число - сдвиг вправо), отрицательное - сдвиг влево):");
+            int n = Action.Input();
+            
+            string newtxt = Action.Encryption(text, n);
+            WriteLine(newtxt);
+            Read();
         }
     }
 }
