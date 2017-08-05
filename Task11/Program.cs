cf258066a6d9291fb299156ade3ba32335df313a
Алексеева Анастасия Николаевна <analekseeva_1@edu.hse.ru>
2017-08-05 13:20:43 +0500

    добавлены комментарии, исправлены мелкие ошибки

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -5,20 +5,20 @@ namespace Task11
 {
     class Action
     {
-        public static readonly string alphabetRU = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-        public static readonly string alphabetENG = "abcdefghijklmnopqrstuvwxyz";
-        public static readonly string symbols = @"!&.,?/\*^$#@№;%:()-_=+1234567890";
+        static readonly string alphabetRU = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"; 
+        static readonly string alphabetENG = "abcdefghijklmnopqrstuvwxyz";
+        static readonly string symbols = @"!&.,?/\*^$#@№;%:()-_=+1234567890";
 
-        public static char Encode(char c, int n, bool lang)
+        static char Encode(char c, int n, bool lang) // кодирование элемента
         {
             return lang
-                ? alphabetRU[(alphabetRU.IndexOf(c) + n) % alphabetRU.Length]
-                : alphabetENG[(alphabetENG.IndexOf(c) + n) % alphabetENG.Length];
+                ? alphabetRU[(alphabetRU.IndexOf(c) + n) % alphabetRU.Length] // для русского алфавита
+                : alphabetENG[(alphabetENG.IndexOf(c) + n) % alphabetENG.Length]; // для английского алфавита
         }
 
-        public static int Input()
+        public static int Input() // ввод числа N
         {
-            int number = 0, lenght = 33;
+            int number = 0;
             bool ok;
             do
             {
@@ -38,25 +38,38 @@ namespace Task11
                     ok = false;
                 }
             } while (!ok);
-
-            number = number % lenght;
-            if (number < 0) number = lenght + number;
             return number;
         }
 
-        public static string Encryption(string text, int n)
+        public static int NConvert(int number, int length) // перевод числа N в корректный формат в зависимости от алфавита
         {
-            string newtxt = String.Empty;
-            
-            bool lang = true;
+            number = number % length;
+            if (number < 0) number = length + number;
+            return number;
+        }
+
+        public static string Encryption(string text, int n) // кодировка
+        {
+            string newtxt = String.Empty; // переменная для закодированного текста
+            int length = 0; // длина алфавита
+            bool lang = true; // переменная для выбора алфавита
             foreach (char t in text)
             {
                 if (!alphabetRU.Contains(t.ToString())) // определение алфавита
                 {
                     lang = false;
+                    length = alphabetENG.Length;
+                    break;
+                }
+                else
+                {
+                    lang = true;
+                    length = alphabetRU.Length;
                     break;
                 }
             }
+
+            n = NConvert(n, length); // перевод N в корректный формат
 
             foreach (char t in text) // зашифровка
             {
@@ -80,8 +93,8 @@ namespace Task11
                               "\nПоложительное число - сдвиг вправо), отрицательное - сдвиг влево):");
             int n = Action.Input();
             
-            string newtxt = Action.Encryption(text, n);
-            WriteLine(newtxt);
+            string newtxt = Action.Encryption(text, n); // зашифровка
+            WriteLine(newtxt); // вывод текста
             Read();
         }
     }
